3111bda42bb0effa126576d33a32300fb0c6858f
Diamond-04 <murtazobahsiev@gmail.com>
2024-03-05 20:29:31 +0500

    firstv

---
+++ index.js
@@ -0,0 +1,74 @@
+//1
+// for (let i = -100; i <=0; i++) {
+//     console.log(i);
+    
+// }
+// //2
+// for (let i = 1; i <=100; i++) {
+//     console.log(i);
+    
+// }
+//3
+// for (let i = 1; i <=100; i++) {
+//     console.log(i);
+    
+// }
+//4
+
+// for (let i = 1; i <=100; i++) {
+//     console.log(i%2===0?i:'нечетное');
+    
+// }
+//5
+// for (let i = 1; i <=100; i++) {
+//    console.log(i%3===0?i:null);
+    
+// }
+//6
+//  let mardum = [];
+//  for (let i = 0; i <=100; i++) {
+//     i %2===0?  mardum.push(i):null;
+    
+
+ 
+
+
+    
+
+    
+   
+    
+//  }
+//  console.log(mardum);
+
+ 
+//7
+// let mardum = []
+// for (let i = 1; i <= 10; i++) {
+//     mardum.push(i)
+
+    
+// }
+//8
+ //let num = [1,2,3,-4,-5,-6]
+// let nums = 0
+// for (let i = 0; i < num.length; i++) {
+//     num[i]<0?nums++ :null;
+    
+// }
+// console.log("Количество отрицательных чисел:",nums);
+// //9
+// let num = [1,2,34,,-45,-67,-78,-89]
+// let allNum = num.filter(nums=> nums>0)
+
+//10
+// let str = 'ige'
+// let news = str.charAt(str.length -2)
+// console.log(news);
+
+
+
+
+let header = document.querySelector('#main-heading')
+let newHeader = prompt('')
+header.innerHTML = newHeader
